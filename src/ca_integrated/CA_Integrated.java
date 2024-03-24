76e6b72403fc18783a601c6302ef03b6b3d3082a
marktimestwo <markescabarte79@yahoo.com>
2024-03-24 23:05:11 +0000

    jdbc connection
    testing out jdbc connection

--- src/ca_integrated/CA_Integrated.java
+++ src/ca_integrated/CA_Integrated.java
@@ -4,19 +4,31 @@
  */
 package ca_integrated;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author marktimestwo
  */
 public class CA_Integrated {
 
-    /**
-     * @param args the command line arguments
-     */
+      
+    private static final String URL = "jdbc:mysql://localhost:3306/Integrated_CA";
+    private static final String USER = "root";
+    private static final String PASSWORD = "@Markcodestoo24";
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        testing!
+        
     }
     
 }
