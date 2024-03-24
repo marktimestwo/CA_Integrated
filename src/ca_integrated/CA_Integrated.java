ebedc35e748c748cf124d1e4e86410c6caf2c283
marktimestwo <markescabarte79@yahoo.com>
2024-03-24 23:03:22 +0000

    testing

--- src/ca_integrated/CA_Integrated.java
+++ src/ca_integrated/CA_Integrated.java
@@ -16,8 +16,7 @@ public class CA_Integrated {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
-        hello test testing
+        testing!
     }
     
 }
