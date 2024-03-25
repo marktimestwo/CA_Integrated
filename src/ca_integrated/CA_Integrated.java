2d00dc4a6e2be0a066357a0192749ce36cd0df0c
marktimestwo <markescabarte79@yahoo.com>
2024-03-25 22:46:13 +0000

    generate report comment
    added comments to generareport method

--- src/ca_integrated/CA_Integrated.java
+++ src/ca_integrated/CA_Integrated.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
  * @author marktimestwo
  */
 public class CA_Integrated {
-     
+    // Database connection to establish and return data from MySQL  
     private static final String URL = "jdbc:mysql://localhost:3306/Integrated_CA";
     private static final String USER = "root";
     private static final String PASSWORD = "@Markcodestoo24";
@@ -29,16 +29,17 @@ public class CA_Integrated {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
     
-    public void generateCourseReport() {
+    // Method to generate and print out course report details
+    public void generateCourseReport() { // SQL query that joins several tables to gather information about the course
         String courseQuery = "SELECT m.Module_Name, p.Programme_Name, COUNT(e.Student_ID) AS Enrolled_Students, "
                 + "l.Name AS Lecturer_Name, r.Room_Name FROM Modules m JOIN Programmes p ON m.Programme_ID = p.Programme_ID "
                 + "LEFT JOIN Enrollments e ON m.Module_ID = e.Module_ID AND e.Status = 'Enrolled' JOIN Module_Lecturers ml ON m.Module_ID = ml.Module_ID "
                 + "JOIN Lecturers l ON ml.Lecturer_ID = l.Lecturer_ID LEFT JOIN Rooms r ON m.Room_ID = r.Room_ID GROUP BY m.Module_ID, l.Name";
-        try (Connection connection = getConnection();
+        try (Connection connection = getConnection(); // Try resource to ensure that the resources are closed after
              PreparedStatement statement = connection.prepareStatement(courseQuery);
              ResultSet resultSet = statement.executeQuery()) {
 
-            while (resultSet.next()) {
+            while (resultSet.next()) { // This iterates over each row and formats the report into a readable string
                 System.out.println(String.format("Module: %s, Programme: %s, Enrolled Students: %d, Lecturer: %s, Room: %s",
                     resultSet.getString("Module_Name"),
                     resultSet.getString("Programme_Name"),
@@ -46,7 +47,7 @@ public class CA_Integrated {
                     resultSet.getString("Lecturer_Name"),
                     resultSet.getString("Room_Name")));
             }
-        } catch (SQLException e) {
+        } catch (SQLException e) { // Catch any exception that may occur
             e.printStackTrace();
         }
     }
