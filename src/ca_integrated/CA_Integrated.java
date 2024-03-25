b312dc3a3111cc6a14cefa27b3ec10c236225566
marktimestwo <markescabarte79@yahoo.com>
2024-03-25 22:11:48 +0000

    Course Report method
    added a course report method to  extract data from MySQL database using MySQL query. Also imported preparedstatement and resultset

--- src/ca_integrated/CA_Integrated.java
+++ src/ca_integrated/CA_Integrated.java
@@ -6,6 +6,8 @@ package ca_integrated;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -22,13 +24,34 @@ public class CA_Integrated {
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+    
+        public void generateCourseReport() {
+        String courseQuery = "SELECT m.Module_Name, p.Programme_Name, COUNT(e.Student_ID) AS Enrolled_Students, "
+                + "l.Name AS Lecturer_Name, r.Room_Name FROM Modules m JOIN Programmes p ON m.Programme_ID = p.Programme_ID "
+                + "LEFT JOIN Enrollments e ON m.Module_ID = e.Module_ID AND e.Status = 'Enrolled' JOIN Module_Lecturers ml ON m.Module_ID = ml.Module_ID "
+                + "JOIN Lecturers l ON ml.Lecturer_ID = l.Lecturer_ID LEFT JOIN Rooms r ON m.Room_ID = r.Room_ID GROUP BY m.Module_ID, l.Name";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(courseQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                System.out.println(String.format("Module: %s, Programme: %s, Enrolled Students: %d, Lecturer: %s, Room: %s",
+                    resultSet.getString("Module_Name"),
+                    resultSet.getString("Programme_Name"),
+                    resultSet.getInt("Enrolled_Students"),
+                    resultSet.getString("Lecturer_Name"),
+                    resultSet.getString("Room_Name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     
     
     
     public static void main(String[] args) {
-        
-        
+          
     }
     
 }
